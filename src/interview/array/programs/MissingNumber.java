57a17cfe2eb4b748eede7c17438b148ba0176bc3
Viyaan Jhiingade <30381782+Viyaan@users.noreply.github.com>
2020-04-19 22:55:41 +0530

    Update MissingNumber.java

--- src/interview/array/programs/MissingNumber.java
+++ src/interview/array/programs/MissingNumber.java
@@ -1,20 +1,22 @@
 package interview.array.programs;
 
+/**
+ * @author Viyaan
+ * 
+ *         You are given a list of n-1 integers and these integers are in the
+ *         range of 1 to n. There are no duplicates in the list. One of the
+ *         integers is missing in the list. 
+ *
+ */
 public class MissingNumber {
-
-	static int getMissingNo(int a[], int n) {
-		int i, total;
-		total = (n + 1) * (n + 2) / 2;
-		for (i = 0; i < n; i++)
-			total -= a[i];
-		return total;
-	}
-
-	/* program to test above function */
 	public static void main(String args[]) {
-		int a[] = { 1, 2, 4, 5, 6 };
-		int miss = getMissingNo(a, a.length);
-		System.out.println(miss);
+		int arr[] = { 1, 2, 4, 5, 6 };
+		
+		int n = arr.length +1;
+		int sum = (n*(n+1))/2;
+		for (int i =0;i<arr.length;i++) {
+			sum = sum - arr[i];
+		}
+		System.out.println(" Missing number is "+sum);
 	}
-
 }
