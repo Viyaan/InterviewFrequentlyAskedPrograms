43b16fa5a390225110589bbc6f1f782d52cff19f
Viyaan Jhiingade <30381782+Viyaan@users.noreply.github.com>
2020-04-24 00:01:12 +0530

    Update MarkerInterfaceDemo.java

--- src/interview/polymorphism/programs/MarkerInterfaceDemo.java
+++ src/interview/polymorphism/programs/MarkerInterfaceDemo.java
@@ -1,5 +1,28 @@
 package interview.polymorphism.programs;
 
+/**
+ * @author Viyaan
+ * 
+ *         In below example, we have created two empty interfaces Cheque and
+ *         BankDraft. And Payment class implemented BankDraft interface. In
+ *         MainClass class both interface behave as tag, output of MainClass
+ *         depends on what interface you have implemented in Payment class.
+ * 
+ *         <pre>
+ *         In below case, 
+ *         public class Payment implements BankDraft
+ *         That’s why output is
+ *         Payment by Draft
+ * 
+ *         If you have change as follows 
+ *         public class Payment implements Cheque
+ * 
+ *         Then new output will be 
+ *         Payment by Cheque
+ * 
+ *         <pre>
+ *
+ */
 public class MarkerInterfaceDemo {
 
 	public static void main(String[] args) {
