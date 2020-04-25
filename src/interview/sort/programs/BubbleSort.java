d3e7dcf53d3ec9033944fa3c9f2de8f0dde4055f
Viyaan Jhiingade <30381782+Viyaan@users.noreply.github.com>
2020-04-25 15:16:09 +0530

    Update BubbleSort.java

--- src/interview/sort/programs/BubbleSort.java
+++ src/interview/sort/programs/BubbleSort.java
@@ -3,17 +3,26 @@ package interview.sort.programs;
 import java.util.Arrays;
 
 public class BubbleSort {
+	
+	private static void sortString(String[] arr, int length) {
+		if(length ==1)
+			return;
+		for(int i =0; i <length-1; i++) {
+			if (arr[i].compareTo(arr[i+1]) > 0) {
+				String temp = arr[i];
+				arr[i] = arr[i+1];
+				arr[i+1]= temp;
+			}
+			sortString(arr,length-1);
+		}
+	}
 
 	// A function to implement bubble sort
 	static void sort(int arr[], int n) {
-		// Base case
 		if (n == 1)
 			return;
-		// One pass of bubble sort. After this pass, the largest element is moved (or
-		// bubbled) to end.
 		for (int i = 0; i < n - 1; i++)
 			if (arr[i] > arr[i + 1]) {
-				// swap arr[i], arr[i+1];
 				int temp = arr[i];
 				arr[i] = arr[i + 1];
 				arr[i + 1] = temp;
@@ -26,5 +35,9 @@ public class BubbleSort {
 		int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
 		sort(arr, arr.length);
 		System.out.println("Sorted array : " + Arrays.toString(arr));
+		
+		String str[] = { "sun", "kite", "rat", "van", "nanny"};
+		sortString(str, str.length);
+		System.out.println("Sorted array : " + Arrays.toString(str));
 	}
 }
