7f831e1792399b299c5cd6bcfadbe314c3d93937
Viyaan Jhiingade <30381782+Viyaan@users.noreply.github.com>
2020-05-09 01:48:38 +0530

    Update InsertionSort.java

--- src/interview/sort/programs/InsertionSort.java
+++ src/interview/sort/programs/InsertionSort.java
@@ -5,20 +5,20 @@ import java.util.Arrays;
 public class InsertionSort {
 
 	public static void main(String[] args) {
-		int arr[] = { 64, 34, 25, 12, 22, 11, 90, 5, 100, 63, -9 };
+		int arr[] = { 64, 34, 25, 12, 22, 11, 90, 5, 100, 63, -9, -1 };
 		sort(arr, arr.length);
 		System.out.println("Sorted array : " + Arrays.toString(arr));
 	}
 
 	static void sort(int arr[], int n) {
-		for (int i = 1; i < n; ++i) {
-			int key = arr[i];
-			int j = i - 1;
-			while (j >= 0 && arr[j] > key) {
-				arr[j + 1] = arr[j];
+		for (int i = 0; i < n; i++) {
+			int j = i;
+			while (j > 0 && arr[j - 1] > arr[j]) {
+				int key = arr[j];
+				arr[j] = arr[j - 1];
+				arr[j - 1] = key;
 				j = j - 1;
 			}
-			arr[j + 1] = key;
 		}
 	}
 }
